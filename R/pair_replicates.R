bad1354d3270bfbe3be6c9d7bdc3d58226b33232
Maciej Grochowski <m.grochowski@student.uw.edu.pl>
2023-07-06 15:57:02 +0200

    Imports > Depends

--- R/pair_replicates.R
+++ R/pair_replicates.R
@@ -1,7 +1,7 @@
 #' Teach R to recognize replicates of independent samples in Genome-wide 3' RACE
 #' @description pair_replicates() creates list of two character vectors, first contains names of used strains, second contains expressions that can be used to grep the said strains from the list of data frames
 #' @param data A list that is an output of gw3RACE::data_fix()
-#' @param grep1 an argument passed to gsub(), regular expression that can be used to erase the indication of replication of experiment from name of data table stored on the list. By default it is "_clone\\\d"
+#' @param grep1 an argument passed to gsub(), regular expression that can be used to erase the indication of replication of experiment from name of data table stored on the list. By default it is "_clone\\\\d"
 #'
 #' @return List of vectors
 #' @export
